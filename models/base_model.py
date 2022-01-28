a5b803e644ad4e66bc29d46ab104f67c6829508d
iChigozirim <iamokwukwe@gmail.com>
2022-01-28 16:25:32 +0100

    Write a class BaseModel that defines all common attributes/methods for other classes:

---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Defines a class Base."""
+from datetime import datetime
+from uuid import uuid4
+from models.engine.file_storage import FileStorage
+#from models import storage
+import models
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the BaseModel class."""
+
+        self.id = str(uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+
+    def __str__(self):
+        """Returns print/str representation of the BaseModel instance."""
+        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates `updated_at` with the current datetime."""
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary containing key/values of self.__dict__"""
+        dictionary = self.__dict__
+        dictionary["__class__"] = __class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
