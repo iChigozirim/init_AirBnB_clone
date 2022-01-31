9d037c319ce5f22e07a8bf668c196fe73e515477
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-01-31 13:26:16 -0800

    Define AirBnB console

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines the AirBnB console. """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the AirBnB command intrepreter.
+    Attributes:
+        prompt (str): The command prompt.
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
