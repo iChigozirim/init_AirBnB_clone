8083f375e1eca851f304964843df7c4307cbc4b6
iChigozirim <iamokwukwe@gmail.com>
2022-02-25 17:45:42 +0100

    Correct errors.

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,25 @@
 #/usr/bin/python3
 """Defines a class Base."""
-from datetime import datetime
-from uuid import uuid4
 import models
+from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize the BaseModel class."""
+        """Initialize the BaseModel class.
+        
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes
+        """
 
         self.id = str(uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
-        models.storage.new(self)
-
+        
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -23,11 +27,13 @@ class BaseModel:
                         setattr(self, key, datetime.fromisoformat(value))
                     else:
                         setattr(self, key, value)
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """Returns print/str representation of the BaseModel instance."""
-        d = self.__dict__.copy()
-        return "[{}] ({}) {}".format(__class__.__name__, self.id, d)
+        d = self.__dict__
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, d)
 
     def save(self):
         """Updates `updated_at` with the current datetime."""
@@ -37,7 +43,7 @@ class BaseModel:
     def to_dict(self):
         """Returns a dictionary containing key/values of self.__dict__"""
         dictionary = self.__dict__.copy()
-        dictionary["__class__"] = __class__.__name__
+        dictionary["__class__"] = self.__class__.__name__
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
